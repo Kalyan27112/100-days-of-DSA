dc08411050547ce5f7a58a697d97be6e7bb9edfe
Kalyan Ram <Thodupunuri.27112@stu.upes.ac.in>
2026-02-20 07:04:20 +0530

    Create Day18_Rotate array by k positions.c

---
+++ Day18_Rotate array by k positions.c
@@ -0,0 +1,23 @@
+//Problem 18. Given an array of integers, rotate the array to the right by k positions.
+
+#include <stdio.h>
+
+void reverse(int arr[], int start, int end) {
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int n, k;
+    int arr[100];
+    
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &k);
