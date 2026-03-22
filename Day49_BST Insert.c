f14600dadd1d752ba5943f292c99b524f040e72d
Kalyan Ram <Thodupunuri.27112@stu.upes.ac.in>
2026-03-22 15:29:14 +0530

    Day49_BST Insert.c

---
+++ Day49_BST Insert.c
@@ -0,0 +1,14 @@
+//Problem 49. BST Insert
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// BST Insert implementation
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    // Implementation here
+    printf("Result\n");
+    return 0;
+}
