4f6ad29a948dd89c633c98fe4e9759d9a2ef2e22
Kalyan Ram <Thodupunuri.27112@stu.upes.ac.in>
2026-04-13 07:51:08 +0530

    Day71_Hash Table Using Quadratic Probing.c

---
+++ Day71_Hash Table Using Quadratic Probing.c
@@ -0,0 +1,44 @@
+//Problem Statement 71.
+Implement a hash table using quadratic probing with formula:
+h(k, i) = (h(k) + i*i) % m
+
+#include <stdio.h>
+
+#define SIZE 7
+
+int table[SIZE];
+
+void init() {
+    for(int i = 0; i < SIZE; i++) {
+        table[i] = -1;
+    }
+}
+
+void insert(int key) {
+    int h = key % SIZE;
+    int i = 0;
+
+    while(i < SIZE) {
+        int index = (h + i*i) % SIZE;
+        if(table[index] == -1) {
+            table[index] = key;
+            return;
+        }
+        i++;
+    }
+}
+
+int search(int key) {
+    int h = key % SIZE;
+
+    for(int i = 0; i < SIZE; i++) {
+        int index = (h + i*i) % SIZE;
+
+        if(table[index] == key)
+            return 1;
+
+        if(table[index] == -1)
+            return 0;
+    }
+    return 0;
+}  
